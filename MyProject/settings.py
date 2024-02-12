436827442bbae17e2c8268e9f8099579f9004a71
Jf-felipe14 <jean-felipe14@hotmail.com>
2024-02-12 11:57:15 -0300

    Adcionando Foreign Key

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contact',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'MyProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -116,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS= [BASE_DIR / 'base_static']
+
+STATIC_ROOT = BASE_DIR / 'static_file'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT =  BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,25 @@
+from typing import Any
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return {self.name}
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank = True)
+    show = models.BooleanField(default = True)
+    picture = models.ImageField(blank=True,upload_to='picture_%Y/%m/%d')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,blank=True,null=True)
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    def __repr__(self) -> str:
+        return f'Nome: {self.first_name}, Sobrenome: {self.last_name}'
